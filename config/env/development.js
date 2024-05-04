2fa3522f1ac5c6e2db4ffc8a4006e839af699693
Bipul Kumar <bknor951@gmail.com>
2024-05-04 18:27:35 +0530

    m

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,4 @@
 module.exports = {
   JWT_EXPIRES_IN: "1d",
   JWT_SECRET: "96FFNw/HkBYSZ+aGU46E8KqXveR7gqczco/z6/g3bEs=",
-  //   TWILIO_ACCOUNT_SID: "AC054494fdaef9ff93168a5ac216f439b5",
-  //   TWILIO_AUTH_TOKEN: "9fa51ac43596dc5dcf311a58d0e90f3e",
 };
